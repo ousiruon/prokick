04833c5282ddd25ccc6624deb51f4c947c2275fd
ousiruon <ou.dehmani@gmail.com>
2025-02-06 17:24:16 -0500

    Repairing Images

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -8,7 +8,7 @@ const Testimonials = () => {
     {
       id: 1,
       name: "Chinedu Nwosu",
-      img: "../../public/players/Chinedu Nwosu.jpg",
+      img: "/players/Chinedu Nwosu.jpg",
       content:
         "ProKick has been nothing short of transformational in my football journey. From the moment I joined, they not only showcased my skills on a global scale but also connected me with top-tier clubs around the world, significantly elevating my career prospects.",
       club: "FC Barcelona",
@@ -16,7 +16,7 @@ const Testimonials = () => {
     {
       id: 2,
       name: "Yuki Nakamura",
-      img: "../../public/players/Yuki Nakamura.jpg",
+      img: "/players/Yuki Nakamura.jpg",
       content:
         "Initially skeptical about ProKick's approach, I quickly realized their expertise and unwavering support would be instrumental in unlocking doors to opportunities previously unimaginable. Their dedication is unmatched.",
       club: "Bayern Munich",
@@ -24,7 +24,7 @@ const Testimonials = () => {
     {
       id: 3,
       name: "Hassan El Fassi",
-      img: "../../public/players/Hassan El Fassi.jpg",
+      img: "/players/Hassan El Fassi.jpg",
       content:
         "ProKick turned my dream of playing football in Europe into reality through their invaluable guidance and relentless support. Every step along the way has been marked by their unwavering commitment, for which I am eternally grateful.",
       club: "Paris Saint-Germain",
@@ -32,7 +32,7 @@ const Testimonials = () => {
     {
       id: 4,
       name: "Lucas Silva",
-      img: "../../public/players/Lucas Silva.jpg",
+      img: "/players/Lucas Silva.jpg",
       content:
         "ProKick's impact on my life is profound. They not only connected me with top clubs in South America but also provided the necessary tools and resources to elevate my game, ensuring my continued growth as a professional athlete.",
       club: "Flamengo",
@@ -40,7 +40,7 @@ const Testimonials = () => {
     {
       id: 5,
       name: "Rahul Sharma",
-      img: "../../public/players/Rahul Sharma.jpg",
+      img: "/public/players/Rahul Sharma.jpg",
       content:
         "ProKick's unwavering dedication towards nurturing young talent has been nothing short of awe-inspiring. They meticulously sought out opportunities for me, eventually securing a position at one of Europe's most prestigious football clubs.",
       club: "Chelsea FC",
